8d3d8ed34c7260afb4f92047598d1ec72bd5a712
hbhalala <37711979+hbhalala@users.noreply.github.com>
2022-12-05 22:47:07 -0600

    Adding more exercises

--- src/test/java/NearHundredTest.java
+++ src/test/java/NearHundredTest.java
@@ -2,6 +2,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a
+number.
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+ */
+
 class NearHundredTest {
 
     NearHundred nh = new NearHundred();
